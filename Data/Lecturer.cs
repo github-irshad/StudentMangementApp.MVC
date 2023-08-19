18da0952a3b7f84b18f2bde8a256c4ada4b97774
github-irshad <irshadaj08@gmail.com>
2023-08-19 20:05:48 +0530

    DB first approach

---
+++ Data/Lecturer.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace StudentMangementApp.MVC.Data;
+
+public partial class Lecturer
+{
+    public int Id { get; set; }
+
+    public string Firstname { get; set; }
+
+    public string Lastname { get; set; }
+
+    public DateOnly? Dateofbirth { get; set; }
+}
---
+++ Data/SchoolmanagementdbContext.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace StudentMangementApp.MVC.Data;
+
+public partial class SchoolmanagementdbContext : DbContext
+{
+    public SchoolmanagementdbContext(DbContextOptions<SchoolmanagementdbContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Course> Courses { get; set; }
+
+    public virtual DbSet<Lecturer> Lecturers { get; set; }
+
+    public virtual DbSet<Student> Students { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Course>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("courses_pkey");
+
+            entity.ToTable("courses");
+
+            entity.HasIndex(e => e.Code, "courses_code_key").IsUnique();
+
+            entity.Property(e => e.Id).HasColumnName("id");
+            entity.Property(e => e.Code)
+                .HasMaxLength(5)
+                .HasColumnName("code");
+            entity.Property(e => e.Credits).HasColumnName("credits");
+            entity.Property(e => e.Name)
+                .IsRequired()
+                .HasMaxLength(50)
+                .HasColumnName("name");
+        });
+
+        modelBuilder.Entity<Lecturer>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("lecturers_pkey");
+
+            entity.ToTable("lecturers");
+
+            entity.Property(e => e.Id).HasColumnName("id");
+            entity.Property(e => e.Dateofbirth).HasColumnName("dateofbirth");
+            entity.Property(e => e.Firstname)
+                .IsRequired()
+                .HasMaxLength(50)
+                .HasColumnName("firstname");
+            entity.Property(e => e.Lastname)
+                .IsRequired()
+                .HasMaxLength(50)
+                .HasColumnName("lastname");
+        });
+
+        modelBuilder.Entity<Student>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("students_pkey");
+
+            entity.ToTable("students");
+
+            entity.Property(e => e.Id).HasColumnName("id");
+            entity.Property(e => e.Dateofbirth).HasColumnName("dateofbirth");
+            entity.Property(e => e.Firstname)
+                .IsRequired()
+                .HasMaxLength(50)
+                .HasColumnName("firstname");
+            entity.Property(e => e.Lastname)
+                .IsRequired()
+                .HasMaxLength(50)
+                .HasColumnName("lastname");
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
